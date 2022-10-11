6d98ed989f654b7d61fe9521416892482c554aa0
mario <manriqueduran123@gmail.com>
2022-10-11 18:35:01 -0500

    structure for side header complete

---
+++ src/adjustments-module.js
@@ -0,0 +1,42 @@
+import { sideHeader } from "./first-load";
+
+import sourcesIconWorld from "./images/world.svg";
+import settingsIconGear from "./images/settings.svg";
+
+export function adjustmentsFunction() {
+    // create obj
+    const adjustments = document.createElement("div");
+
+    const sources = document.createElement("div");
+    const sourcesIcon = document.createElement("img");
+    const sourcesText = document.createElement("p");
+    sourcesIcon.src = sourcesIconWorld;
+    sourcesText.textContent = "Sources";
+
+    const settings = document.createElement("div");
+    const settingsIcon = document.createElement("img");
+    const settingsText = document.createElement("p");
+    settingsIcon.src = settingsIconGear;
+    settingsText.textContent = "Settings";
+
+    // add classes
+    adjustments.classList.add("adjustments");
+
+    sources.classList.add("sources");
+    sourcesIcon.classList.add("icon");
+    sourcesText.classList.add("text");
+
+    settings.classList.add("settings");
+    settingsIcon.classList.add("icon");
+    settingsText.classList.add("text");
+
+    // append
+    sideHeader.insertAdjacentElement("beforeend", adjustments); 
+    adjustments.insertAdjacentElement("beforeend", sources);
+    adjustments.insertAdjacentElement("beforeend", settings);
+    sources.insertAdjacentElement("beforeend", sourcesIcon);
+    sources.insertAdjacentElement("beforeend", sourcesText);
+    settings.insertAdjacentElement("beforeend", settingsIcon);
+    settings.insertAdjacentElement("beforeend", settingsText);
+
+}
--- src/first-load.js
+++ src/first-load.js
@@ -1,32 +1,25 @@
 import { todoList } from "./todo-list-module";
 import { prioritiesFuction } from "./priorities-module";
 import { projectsFunction } from "./projects-module";
+import { adjustmentsFunction } from "./adjustments-module";
 
 const content = document.querySelector("#content");
 const sideHeader = document.createElement("div");
+const main = document.createElement("div");
 
 export function firstLoad() {
-    // creating objects
-
-    const adjustments = document.createElement("div");
-    
-    const main = document.createElement("div");
-
     // adding classes
     sideHeader.classList.add("side-header");
-
-    adjustments.classList.add("adjustments");
-
     main.classList.add("main");
 
     // appending
     content.insertAdjacentElement("beforeend", sideHeader);
-    content.insertAdjacentElement("beforeend", main);
-    sideHeader.insertAdjacentElement("beforeend", adjustments);    
+    content.insertAdjacentElement("beforeend", main);   
 }
 
 todoList();
 prioritiesFuction();
 projectsFunction();
+adjustmentsFunction();
 
-export {sideHeader};
+export {sideHeader, main};
